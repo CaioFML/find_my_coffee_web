20a70e17cc1d845875be82c6135c60c39e6c2f61
CaioFML <caio.felipemotta@hotmail.com>
2020-10-28 22:37:36 -0300

    Criando a sessão de cafés mais próximos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import Establishment from './components/Establishment'
+import NearstCoffees from './components/NearstCoffees';
 
 import EstablishmentsService from './services/establishment_service'
 
@@ -36,28 +37,32 @@ function App() {
   return (
     <Fragment>
       <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_API_KEY}>
-        <GoogleMap mapContainerStyle={{height: "100vh", width: "100%"}}
+        <GoogleMap mapContainerStyle={{ height: "100vh", width: "100%" }}
           zoom={15}
-          center={{lat: latitude, lng: longitude}}>
-            {
-              locations.map((item, index) => {
-                return (
-                  <Marker key={index} icon="/images/coffee-pin.png" title={item.name} animation="4"
-                    position={{lat: item.geometry.location.lat, lng: item.geometry.location.lng}}
-                    onClick={() => setSelected(item)}
-                  />
-                )
-              })
-            }
+          center={{ lat: latitude, lng: longitude }}>
+          {
+            locations.map((item, index) => {
+              return (
+                <Marker key={index} icon="/images/coffee-pin.png" title={item.name} animation="4"
+                  position={{ lat: item.geometry.location.lat, lng: item.geometry.location.lng }}
+                  onClick={() => setSelected(item)}
+                />
+              )
+            })
+          }
           {
             selected.place_id && (
-              <Establishment place={selected}/>
+              <Establishment place={selected} />
             )
           }
 
           <Marker key="my location" icon="/images/my-location-pin.png" title="Seu local"
-                    animation="2" position={{lat: latitude, lng: longitude}}
+            animation="2" position={{ lat: latitude, lng: longitude }}
           />
+
+          {(latitude != 0 && longitude != 0) &&
+            <NearstCoffees latitude={latitude} longitude={longitude} />
+          }
         </GoogleMap>
       </LoadScript>
     </Fragment>
--- src/services/store.js
+++ src/services/store.js
@@ -2,7 +2,7 @@ import Api from './api';
 
 const StoreService = {
   show: (google_place_id) => Api.get(`/stores/${google_place_id}`),
-  index: (latitude, longitude) => Api.get('/stores', {params: {latitude: latitude, longitude: longitude}}),
+  index: (longitude, latitude) => Api.get('/stores', {params: {latitude: latitude, longitude: longitude}}),
 }
 
 export default StoreService
